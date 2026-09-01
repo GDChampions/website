707f3d2119c4c96e45833d33b2afd9bc166fce5c
demibabs <demibabs@gmail.com>
2026-09-01 02:12:02 -0500

    Add compose font and button styling

--- src/home/Hero.tsx
+++ src/home/Hero.tsx
@@ -1,8 +1,9 @@
-import { useRef, useState } from "react";
+import { useRef, useState, type ReactNode } from "react";
 import { cn } from "../utils/cn";
 
 export function Hero() {
-  const buttonClassName = "p-5 text-3xl bg-accent-purple text-accent-yellow font-semibold rounded-md";
+  const buttonClassName =
+    "overflow-hidden relative p-5 text-3xl font-semibold rounded-md drop-shadow-lg border-t-3 border-l-3 border-base-text/30";
   const loopingVideo = useRef<HTMLVideoElement>(null);
   const [loopingVideoIsPlaying, setLoopingVideoIsPlaying] = useState(false);
 
@@ -36,22 +37,52 @@ export function Hero() {
         }}
       ></video>
       {/* Background blur overlay */}
-      <div className="absolute inset-0 bg-black/30 backdrop-blur-md -z-10" />
+      <div className="absolute inset-0 bg-black/30 backdrop-blur-xl -z-10" />
       <hgroup className="contents">
-        <h1 className="w-xl aspect-504/205">
+        <h1 className="w-xl aspect-504/205 drop-shadow-2xl">
           <img
             className="object-contain"
             src="/src/assets/home/hero/full_logo.webp"
           ></img>
         </h1>
-        <p className="text-2xl font-bold">
-          A Geometry Dash mod for real-time ranked competitive play.
+        <p className="text-2xl font-bold text-white">
+          Competitive Geometry Dash is here. Play now!
         </p>
       </hgroup>
       <nav className="flex flex-row gap-5">
-        <button className={buttonClassName}>Install</button>
-        <button className={buttonClassName}>Leaderboard</button>
+        <Button color="pink">Install</Button>
+        <Button color="yellow">Leaderboard</Button>
       </nav>
     </header>
+  );
+}
+
+function Button({
+  color,
+  children,
+}: {
+  color: "yellow" | "pink";
+  children: ReactNode;
+}) {
+  return (
+    <button
+      className={cn(
+        "overflow-hidden relative p-5 text-3xl font-semibold rounded-md drop-shadow-lg border-t-3 border-l-3 border-base-text/30",
+        { "bg-accent-pink text-accent-pink-text": color === "pink" },
+        { "bg-accent-yellow text-accent-yellow-text": color === "yellow" },
+      )}
+    >
+      <span className="absolute inset-0 border-r-3 border-b-3 rounded-br-md opacity-70" />
+      <span
+        className={cn(
+          "absolute inset-0 bg-linear-to-r opacity-50",
+          {
+            "from-accent-pink to-accent-purple": color === "pink",
+          },
+          { "from-accent-yellow to-accent-orange": color === "yellow" },
+        )}
+      />
+      <span className="relative z-10">{children}</span>
+    </button>
   );
 }
